514d6ccf04d9c714a0161fef279870a90843dbc0
weiluqiang <523288310@qq.com>
2020-11-24 10:59:31 +0800

    二叉树的序列化与反序列化

--- src/ai/yunxi/tree/SerializeTree.java
+++ src/ai/yunxi/tree/SerializeTree.java
@@ -131,24 +131,7 @@ public class SerializeTree {
     }
 
     public static void main(String[] args) {
-        TreeNode head = new TreeNode(1);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n3 = new TreeNode(3);
-        TreeNode n4 = new TreeNode(4);
-        TreeNode n5 = new TreeNode(5);
-        TreeNode n6 = new TreeNode(6);
-        TreeNode n7 = new TreeNode(7);
-        TreeNode n8 = new TreeNode(8);
-        TreeNode n9 = new TreeNode(9);
-        head.left = n2;
-        head.right = n3;
-        n2.left = n4;
-        n4.left = n7;
-        n4.right = n8;
-        n3.left = n5;
-        n3.right = n6;
-        n7.left = n9;
-
+        TreeNode head = TreeNode.getDefault();
         SerializeTree serialize = new SerializeTree();
         String str = serialize.serialize(head);
         System.out.println(str);
