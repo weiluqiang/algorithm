bd3690577b3022189d3bff1af6aef17daa5e964c
weiluqiang <523288310@qq.com>
2020-12-23 09:58:53 +0800

    工具类修改

--- src/ai/yunxi/tree/pojo/TreeUtil.java
+++ src/ai/yunxi/tree/pojo/TreeUtil.java
@@ -47,4 +47,14 @@ public class TreeUtil {
         TreeNode n2 = new TreeNode(2, n4, n5);
         return new TreeNode(1, n2, n3);
     }
+
+    public static TreeNode getDefault4() {
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n6 = new TreeNode(2);
+        TreeNode n5 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(1);
+        TreeNode n3 = new TreeNode(6, n6, n7);
+        TreeNode n2 = new TreeNode(5, n4, n5);
+        return new TreeNode(4, n2, n3);
+    }
 }
